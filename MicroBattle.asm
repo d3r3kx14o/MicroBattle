65d3e3e027789bed5b0280d59fa54c4d619498e5
Travis-Xiao <travis.xiao@gmail.com>
2015-03-31 16:04:50 +0800

    Merge changes

--- MicroBattle.asm
+++ MicroBattle.asm
@@ -4,7 +4,6 @@ option casemap :none
 
 include MicroBattle.inc
 
-
 ; #########################################################################
 
 .code 
@@ -29,108 +28,107 @@ WinMain proc hInst     :DWORD,
              CmdLine   :DWORD,
              CmdShow   :DWORD
 
-      ;====================
-      ; Put LOCALs on stack
-      ;====================
+    ;====================
+    ; Put LOCALs on stack
+    ;====================
 
-      LOCAL wc   :WNDCLASSEX
-      LOCAL msg  :MSG
-      LOCAL Wwd  :DWORD
-      LOCAL Wht  :DWORD
-      LOCAL Wtx  :DWORD
-      LOCAL Wty  :DWORD
+    LOCAL wc    :WNDCLASSEX
+    LOCAL msg   :MSG
+    LOCAL Wwd   :DWORD
+    LOCAL Wht   :DWORD
+    LOCAL Wtx   :DWORD
+    LOCAL Wty   :DWORD
 
-      LOCAL Ps     :PAINTSTRUCT
+    LOCAL Ps    :PAINTSTRUCT
 
-      ;==================================================
-      ; Fill WNDCLASSEX structure with required variables
-      ;==================================================
+    ;==================================================
+    ; Fill WNDCLASSEX structure with required variables
+    ;==================================================
 
-      invoke LoadIcon,hInst,500    ; icon ID
-      mov hIcon, eax
+    invoke LoadIcon,hInst,500    ; icon ID
+    mov hIcon, eax
 
-	  szText szClassName,"Project_Class"
+    szText szClassName,"Project_Class"
 
-      mov wc.cbSize,         sizeof WNDCLASSEX
-      mov wc.style,          CS_BYTEALIGNWINDOW
-      mov wc.lpfnWndProc,    offset WndProc
-      mov wc.cbClsExtra,     NULL
-      mov wc.cbWndExtra,     NULL
-      m2m wc.hInstance,      hInst
-      mov wc.hbrBackground,  COLOR_BTNFACE+1
-      mov wc.lpszMenuName,   NULL
-      mov wc.lpszClassName,  offset szClassName
-      m2m wc.hIcon,          hIcon
-      invoke LoadCursor,NULL,IDC_ARROW
-      mov wc.hCursor,        eax
-      m2m wc.hIconSm,        hIcon
+    mov wc.cbSize,         sizeof WNDCLASSEX
+    mov wc.style,          CS_BYTEALIGNWINDOW
+    mov wc.lpfnWndProc,    offset WndProc
+    mov wc.cbClsExtra,     NULL
+    mov wc.cbWndExtra,     NULL
+    m2m wc.hInstance,      hInst
+    mov wc.hbrBackground,  COLOR_BTNFACE+1
+    mov wc.lpszMenuName,   NULL
+    mov wc.lpszClassName,  offset szClassName
+    m2m wc.hIcon,          hIcon
+    invoke LoadCursor,NULL,IDC_ARROW
+    mov wc.hCursor,        eax
+    m2m wc.hIconSm,        hIcon
 
-      invoke RegisterClassEx, ADDR wc
+    invoke RegisterClassEx, ADDR wc
 
-      ;================================
-      ; Centre window at following size
-      ;================================
+    ;================================
+    ; Centre window at following size
+    ;================================
 
-      m2m Wwd, WindowWidth
-      m2m Wht, WindowHeight
+    m2m Wwd, WindowWidth
+    m2m Wht, WindowHeight
 
-      invoke GetSystemMetrics,SM_CXSCREEN
-      invoke TopXY,Wwd,eax
-      mov Wtx, eax
+    invoke GetSystemMetrics,SM_CXSCREEN
+    invoke TopXY,Wwd,eax
+    mov Wtx, eax
 
-      invoke GetSystemMetrics,SM_CYSCREEN
-      invoke TopXY,Wht,eax
-      mov Wty, eax
+    invoke GetSystemMetrics,SM_CYSCREEN
+    invoke TopXY,Wht,eax
+    mov Wty, eax
 
-      invoke CreateWindowEx,WS_EX_LEFT,
-                            ADDR szClassName,
-                            ADDR gameDisplayName,
-                            WS_OVERLAPPEDWINDOW,
-                            Wtx,Wty,Wwd,Wht,
-                            NULL,NULL,
-                            hInst,NULL
-      mov   hWnd,eax
+    invoke CreateWindowEx,WS_EX_LEFT,
+        ADDR szClassName,
+        ADDR gameDisplayName,
+        WS_OVERLAPPEDWINDOW,
+        Wtx,Wty,Wwd,Wht,
+        NULL,NULL,
+        hInst,NULL
+    mov   hWnd,eax
 
-      invoke LoadMenu,hInst,600  ; menu ID
-      invoke SetMenu,hWnd,eax
+    invoke LoadMenu,hInst,600  ; menu ID
+    invoke SetMenu,hWnd,eax
 
-      invoke ShowWindow,hWnd,SW_SHOWNORMAL
-      invoke UpdateWindow,hWnd
+    invoke ShowWindow,hWnd,SW_SHOWNORMAL
+    invoke UpdateWindow,hWnd
 
-      ;===================================
-      ; Set the Timer Event
-      ;===================================
-	
-	invoke SetTimer,hWnd,NULL,GameTimerValue,NULL
+    ;===================================
+    ; Set the Timer Event
+    ;===================================
 
-	m2m hWin,hWnd
+    invoke SetTimer,hWnd,NULL,GameTimerValue,NULL
+
+    m2m hWin,hWnd
 
 
-      ;===================================
-      ; Loop until PostQuitMessage is sent
-      ;===================================
+    ;===================================
+    ; Loop until PostQuitMessage is sent
+    ;===================================
 
     StartLoop:
-      invoke GetMessage,ADDR msg,NULL,0,0
-      cmp eax, 0
-      je ExitLoop
-      	invoke TranslateMessage, ADDR msg
-      	invoke DispatchMessage,  ADDR msg
-      jmp StartLoop
+        invoke GetMessage,ADDR msg,NULL,0,0
+        cmp eax, 0
+        je ExitLoop
+        invoke TranslateMessage, ADDR msg
+        invoke DispatchMessage,  ADDR msg
+        jmp StartLoop
     ExitLoop:
 
-        
-
-      return msg.wParam
+    mov eax, msg.wParam
+    ret
 
 WinMain endp
 
 ; #########################################################################
 
-WndProc proc hWinL  :DWORD,
-	     uMsg   :DWORD,
-	     wParam :DWORD,
-	     lParam :DWORD
+WndProc proc    hWinL  :DWORD,
+                uMsg   :DWORD,
+                wParam :DWORD,
+                lParam :DWORD
 
     LOCAL Ps	:PAINTSTRUCT
 
@@ -141,34 +139,40 @@ WndProc proc hWinL  :DWORD,
     .elseif uMsg == WM_SIZE
 
     .elseif uMsg == WM_PAINT
-	invoke BeginPaint, hWin, addr Ps
-	mov hDC, eax
-	invoke Paint_Proc
-	invoke EndPaint, hWin, addr Ps
-	return 0
+        invoke BeginPaint, hWin, addr Ps
+        mov hDC, eax
+        invoke Paint_Proc
+        invoke EndPaint, hWin, addr Ps
+        mov eax, 0
+        ret
+
     .elseif uMsg == WM_TIMER
-	invoke GameTimer
-	invoke RedrawWindow, hWin, NULL, NULL, RDW_INVALIDATE
+    	invoke GameTimer
+    	invoke RedrawWindow, hWin, NULL, NULL, RDW_INVALIDATE
 
     .elseif uMsg == WM_KEYDOWN
     	.if wParam == VK_A
-	    mov eax, players.Players[0].state
-	    .if eax == 0
-		invoke sndPlaySound, addr shootMini, SND_ASYNC
-	    .endif
-	    ;Player 1 presses the key
-	    mov eax, players.Players[0].speed
-	    neg eax
-	    mov players.Players[0].speed, eax
+			mov ebx, players.Players[0].state
+            ;Player 1 presses the key
+            .if ebx == 0            
+    			invoke FireBullet, 0
+            .elseif ebx == 1
+                mov eax, players.Players[0].speed
+                neg eax
+                mov players.Players[0].speed, eax
+            .elseif ebx == 2
+            .endif
     	.elseif wParam == VK_L
-	    mov eax, players.Players[SIZEOF Player].state
-	    .if eax == 0
-		invoke sndPlaySound, addr shootMini, SND_ASYNC
-	    .endif
-	    ;Player 2 presses the key
-	    mov eax, players.Players[SIZEOF Player].speed
-	    neg eax
-	    mov players.Players[SIZEOF Player].speed, eax
+            ;Player 2 presses the key
+			mov ebx, players.Players[SIZEOF Player].state
+            .if ebx == 0
+        		invoke FireBullet, 1
+            .elseif ebx == 1
+                mov eax, players.Players[SIZEOF Player].speed
+                neg eax
+                mov players.Players[SIZEOF Player].speed, eax
+            .elseif ebx == 2
+            .endif
     	.endif
     .endif
 
@@ -200,6 +204,9 @@ LoadGraphics proc
     invoke LoadBitmap, hInstance, RC_PLAYER2MASK
     mov hPlayer2Mask, eax
 
+    invoke LoadBitmap, hInstance, RC_BULLET
+    mov hBullet, eax
+
     ret
     
 LoadGraphics endp
@@ -210,7 +217,7 @@ Paint_Proc proc
 
     LOCAL memDC:DWORD
     LOCAL hBmp:DWORD
-	LOCAL playerX: DWORD
+    LOCAL playerX: DWORD
 
 invoke CreateCompatibleDC, hDC
     mov memDC, eax
@@ -236,42 +243,57 @@ L1:
     mov eax, (Player PTR [ebx]).state
     ;### for player1
     .if ecx == 2
-	;## if normal
-	.if eax == 0
-		m2m (Player PTR [ebx]).hPlayer, hPlayer1
-		m2m (Player PTR [ebx]).hPlayerMask, hPlayer1Mask
-	;## if player has shot
-	;.elseif eax == 1
-	;## if player got shot
-	;.elseif eax == 2
-	;## if dead
-	;.elseif eax == 3
-	.endif
+    ;## if normal
+    .if eax == 0
+        m2m (Player PTR [ebx]).hPlayer, hPlayer1
+        m2m (Player PTR [ebx]).hPlayerMask, hPlayer1Mask
+    ;## if player has shot
+    ;.elseif eax == 1
+    ;## if player got shot
+    ;.elseif eax == 2
+    ;## if dead
+    ;.elseif eax == 3
+    .endif
     ;### for player2
     .elseif ecx == 1
-	;## if normal
-	.if eax == 0
-	    m2m (Player PTR [ebx]).hPlayer, hPlayer2
-	    m2m (Player PTR [ebx]).hPlayerMask, hPlayer2Mask
-	;## if player has shot
-	;.elseif eax == 1
-	;## if player got shot
-	;.elseif eax == 2
-	;## if dead
-	;.elseif eax == 3
-	.endif
+    ;## if normal
+    .if eax == 0
+        m2m (Player PTR [ebx]).hPlayer, hPlayer2
+        m2m (Player PTR [ebx]).hPlayerMask, hPlayer2Mask
+    ;## if player has shot
+    ;.elseif eax == 1
+    ;## if player got shot
+    ;.elseif eax == 2
+    ;## if dead
+    ;.elseif eax == 3
+    .endif
     .endif
 
     invoke PaintBMPMask,
         (Player PTR [ebx]).hPlayer,
-		(Player PTR [ebx]).hPlayerMask,
-		playerX,
+        (Player PTR [ebx]).hPlayerMask,
+        playerX,
         (Player PTR [ebx]).p_y,
         (Player PTR [ebx]).playerWidth,
         (Player PTR [ebx]).playerHeight
     add ebx, SIZEOF Player
     m2m playerX, PlaygroundWidth
     loop L1
+
+    ;#### Paint bullets
+    mov ecx, bullets.len
+    mov ebx, OFFSET bullets.bullets[0]
+
+    .while ecx > 0
+        invoke PaintBMP,
+            (Bullet PTR [ebx]).hBullet,
+            (Bullet PTR [ebx]).b_x,
+            (Bullet PTR [ebx]).b_y,
+            (Bullet PTR [ebx]).bulletWidth,
+            (Bullet PTR [ebx]).bulletHeight
+        add ebx, SIZEOF Bullet
+        dec ecx
+    .endw
 
     pop ebx
     push ecx
@@ -281,18 +303,18 @@ L1:
   invoke DeleteDC, memDC
   invoke DeleteObject,hBmp
 
-	ret  
+    ret  
 
 Paint_Proc endp
 
 ; #########################################################################
 
 PaintBMP proc uses ecx edi,
-		  BmpHandle:DWORD,
-	      PosX:DWORD,
-	      PosY:DWORD,
-	      BmpW:DWORD,
-	      BmpH:DWORD,
+		  BmpHandle :DWORD,
+	      PosX :DWORD,
+	      PosY :DWORD,
+	      BmpW :DWORD,
+	      BmpH :DWORD,
 
     LOCAL memDC:DWORD
 
@@ -303,17 +325,71 @@ PaintBMP proc uses ecx edi,
     invoke BitBlt, hDC2, PosX, PosY, BmpW, BmpH, memDC, 0, 0, SRCCOPY
     invoke DeleteDC, memDC
 
-    return 0
+    mov eax, 0
+	ret
 
 PaintBMP endp
 
 ; #########################################################################
     
 GameTimer proc
+    invoke MoveBullets
 	invoke MovePlayers
-
 	ret
 GameTimer endp
+
+; #########################################################################
+
+MoveBullets proc
+    pushad
+    mov ecx, bullets.len
+    mov edi, OFFSET bullets.bullets[0]
+
+	.while ecx > 0
+		mov eax, (Bullet PTR [edi]).b_x
+		add eax, (Bullet PTR [edi]).speed_x
+		mov (Bullet PTR [edi]).b_x, eax
+
+		mov eax, (Bullet PTR [edi]).b_y
+		add eax, (Bullet PTR [edi]).speed_y
+		mov (Bullet PTR [edi]).b_y, eax
+		dec ecx
+    .endw
+
+    popad
+    ret
+
+MoveBullets endp
+
+; #########################################################################
+
+FireBullet  proc player :DWORD
+    pushad
+    invoke sndPlaySound, addr shootMini, SND_ASYNC
+    mov edi, OFFSET bullets.bullets[0]
+    mov eax, SIZEOF Bullet
+    mul bullets.len
+    add edi, eax
+	.if player == 0
+		mov ecx, offset players.Players[0]
+	.elseif
+		mov ecx, offset players.Players[SIZEOF Player]
+	.endif
+
+    m2m (Bullet PTR [edi]).hBullet, hBullet
+	mov eax, (Player PTR [ecx]).p_y
+    mov (Bullet PTR [edi]).b_y, eax
+	mov eax, (Player PTR [ecx]).p_x
+    mov (Bullet PTR [edi]).b_x, eax
+    .if eax > 500
+        mov (Bullet PTR [edi]).speed_x, -5
+    .endif
+
+    inc bullets.len
+
+    popad
+    ret
+FireBullet  endp
 
 ; #########################################################################
 
@@ -355,11 +431,11 @@ TopXY endp
 ; ######################################################################### 
 
 PaintBMPMask proc BmpHandle:DWORD,
-		  BmpHandleMask:DWORD, 
-		  PosX:DWORD,
-		  PosY:DWORD,
-		  BmpW:DWORD,
-		  BmpH:DWORD
+          BmpHandleMask:DWORD, 
+          PosX:DWORD,
+          PosY:DWORD,
+          BmpW:DWORD,
+          BmpH:DWORD
     
 
     LOCAL memDC:DWORD
@@ -381,7 +457,7 @@ PaintBMPMask proc BmpHandle:DWORD,
     return 0
 
 PaintBMPMask endp
-; ######################################################################### 
 
+; ######################################################################### 
 
 END start
